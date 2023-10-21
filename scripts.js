6ac6cc139e141ba87166fa09f7e466d08b3b41b6
mjay0018 <135385203+mjay0018@users.noreply.github.com>
2023-10-20 21:51:32 -0400

    changes tooltip display

--- scripts.js
+++ scripts.js
@@ -27,19 +27,29 @@
     const checkboxList = document.getElementById('checkbox-list');
     const completeMessage = document.getElementById('complete-message');
     const addButton = document.getElementById('add-button');
-    const doneButton = document.getElementById('done-button');
+    const completeButton = document.getElementById('complete-button');
     const resetButton = document.getElementById('reset-button');
     const newCheckboxLabelInput = document.getElementById('new-checkbox-label');
     const counterDiv = document.getElementById('counter');
     const counterContainer = document.getElementById('counter-container');
-    const buttonWithTooltip = document.querySelector('.button-with-tooltip');
-    const tooltip = buttonWithTooltip.querySelector('.tooltip');
     const renameButton = document.getElementById('rename-button');
+    const addTooltip = document.getElementById('add-button-tooltip');
+    const completeTooltip = document.getElementById('complete-button-tooltip');
+    const errorMessage = document.getElementById('error-message');
+    
+    function updateProgressBar() {
+      const checkboxContainers = document.querySelectorAll('.checkbox-container');
+      const completedTasks = Array.from(checkboxContainers).filter(container => container.classList.contains('completed'));
+      
+      const percentageCompleted = (completedTasks.length / checkboxContainers.length) * 100;
+      document.getElementById('progress-bar').style.width = `${percentageCompleted}%`;
+  }
+
+
     renameButton.addEventListener('click', toggleRenameMode);
 
   // Initial values for task number and tooltip timeout
     let taskNumber=1;
-    let tooltipTimeout;
 
   // Toggle timer visibility
     const timerToggleButton = document.getElementById('timer-toggle-button');
@@ -63,21 +73,10 @@
         newCheckboxLabelInput.value = '';
         newCheckboxLabelInput.style.display = 'none';
         taskNumber++;
+
     }
+    
     }
-
-     // Event listeners for tooltip show/hide
-    buttonWithTooltip.addEventListener('mouseover', () => {
-        tooltip.classList.remove('tooltip-hidden');
-        tooltipTimeout = setTimeout(() => {
-            tooltip.classList.add('tooltip-hidden');
-        }, 2000);  // 2 seconds
-      });
-
-
-    buttonWithTooltip.addEventListener('mouseout', () => {
-        clearTimeout(tooltipTimeout);
-      });
       function handleBlur(event) {
     if (event.target.value.trim() === '') {
         newCheckboxLabelInput.style.display = 'none';
@@ -185,31 +184,81 @@ document.addEventListener('click', (event) => {
     }
 });
 
+var errorN = 1;
 
-// Logic for marking tasks as done
-    doneButton.addEventListener('click', () => {
-      const checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
-      if (checkboxes.some(checkbox => checkbox.checked)) {
-        counter++;
-        updateCounter();
-      } else {
-        
-    // Show the alert if no tasks completed
-    const popupMessage = document.getElementById('popup-message');
-    popupMessage.classList.remove('hidden');
 
-  // Hide the pop-up after 5 seconds
-    setTimeout(() => {
-    popupMessage.classList.add('hidden');
-    }, 5000);
 
-      }
 
-      checkboxes.forEach(checkbox => {
-        checkbox.checked = false;
+addButton.addEventListener('mouseover', () => {
+  if(errorN==1){
+  addTooltip.textContent = 'Create new task';
+  addTooltip.style.display = 'block';
+}else{
+  addTooltip.style.display = 'none';
+}
+  });
+
+addButton.addEventListener('mouseout', () => {
+  addTooltip.style.display = 'none';
+});
+
+completeButton.addEventListener('mouseover', () => {
+  if(errorN==1){
+  completeTooltip.innerHTML = 'Complete Cycle<br>(complete all available tasks)';
+  completeTooltip.style.display = 'flex';
+}else{
+  completeTooltip.style.display = 'none';
+}
+  });
+
+completeButton.addEventListener('mouseout', () => {
+  completeTooltip.style.display = 'none';
+});
+
+
+
+completeButton.addEventListener('click', () => {
+  const checkboxContainers = document.querySelectorAll('.checkbox-container');
+  
+  // Find all tasks that are marked as completed
+  const completedTasks = Array.from(checkboxContainers).filter(container => container.classList.contains('completed'));
+
+  // If at least one task is completed
+  if (completedTasks.length > 0) {
+      // Mark all tasks as completed
+      checkboxContainers.forEach(container => {
+          container.classList.add('completed');
       });
-      completeMessage.style.display = 'none';
-    });
+      updateProgressBar();
+
+      // Increment the counter since all tasks were completed together
+      counter += 1;
+      updateCounter();
+
+      // Display a "complete" message
+      completeMessage.style.display = 'block';
+
+      // After a short delay, reset all tasks to the uncompleted state
+      setTimeout(() => {
+          checkboxContainers.forEach(container => {
+              container.classList.remove('completed');
+          });
+          updateProgressBar();
+
+          completeMessage.style.display = 'none';  // Hide the "complete" message
+      }, 1000);
+  } else {
+      errorN = 0;
+      completeTooltip.style.display = 'none';
+      // If no tasks are completed, show an error message
+      errorMessage.textContent = 'No tasks selected for completion';
+      errorMessage.style.display = 'block';
+      setTimeout(() => {
+          errorMessage.style.display = 'none';
+          errorN = 1;
+      }, 5000);  // Hide the error message after 5 seconds
+  }
+});
 
      // Logic for resetting task counter
     resetButton.addEventListener('click', () => {
@@ -223,9 +272,10 @@ document.addEventListener('click', (event) => {
     checkboxContainer.className = 'checkbox-container';
     checkboxContainer.id = id + '-container';
 
-    const checkboxInput = document.createElement('input');
-    checkboxInput.type = 'checkbox';
-    checkboxInput.id = id;
+    /*const checkboxDiv = document.createElement('div');
+    checkboxDiv.id = id;
+    checkboxDiv.className = 'checkbox-div';  // You can name this class as you prefer
+    */
 
     const checkboxLabel = document.createElement('label');
     checkboxLabel.setAttribute('for', id);
@@ -246,13 +296,19 @@ document.addEventListener('click', (event) => {
     renameIcon.addEventListener('click', () => { renameTask(id); });
 
     checkboxContainer.appendChild(renameIcon);
-    checkboxContainer.appendChild(checkboxInput);
+    /*checkboxContainer.appendChild(checkboxDiv);*/
     checkboxContainer.appendChild(checkboxLabel);
     checkboxContainer.appendChild(removeButton);
-
     checkboxList.appendChild(checkboxContainer);
     updateDeleteButtonVisibility(); 
-    checkboxInput.addEventListener('change', checkCompletion);
+
+    checkboxContainer.addEventListener('click', () => {
+      checkboxContainer.classList.toggle('completed');
+      updateProgressBar();
+      checkCompletion(); 
+    });
+    updateProgressBar();
+
   }
 
 
@@ -285,23 +341,60 @@ document.addEventListener('click', (event) => {
       checkCompletion();
     }
 
-    // Check if all tasks are completed
+   // Check if all tasks are completed
     function checkCompletion() {
-      const checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
-      if (checkboxes.length > 0 && checkboxes.every(checkbox => checkbox.checked)) {
-        counter++;
-        updateCounter();
-        completeMessage.style.display = 'block';
-        setTimeout(() => {
-          checkboxes.forEach(checkbox => {
-            checkbox.checked = false;
+   const checkboxContainers = document.querySelectorAll('#checkbox-list .checkbox-container');
+    if (checkboxContainers.length > 0 && Array.from(checkboxContainers).every(container => container.classList.contains('completed'))) {
+      counter++;
+      updateCounter();
+      completeMessage.style.display = 'block';
+      setTimeout(() => {
+          Array.from(checkboxContainers).forEach(container => {
+              container.classList.remove('completed');
           });
           completeMessage.style.display = 'none';
-        }, 2000);
-      }
-    }
-    
+          updateProgressBar();  // Reset the progress bar
+      }, 1000);
+  }
+  }
     // Initial setup for counter and button visibility
     updateCounter();
     updateDeleteButtonVisibility();
+
+    document.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+      if (e.target.closest('.checkbox-container')) {
+          contextMenu.style.left = `${e.pageX}px`;
+          contextMenu.style.top = `${e.pageY}px`;
+          contextMenu.style.display = 'block';
+      }
+  });
+
+  const contextMenu = document.getElementById('contextMenu');
+  const renameOption = document.getElementById('renameOption');
+  const deleteOption = document.getElementById('deleteOption');
+
+  renameOption.addEventListener('event', () => {
+      const checkboxContainer = event.target.closest('.checkbox-container');
+      if (checkboxContainer) {
+          const id = checkboxContainer.id.replace('-container', '');
+          renameTask(id);
+          contextMenu.style.display = 'none';  // Hide context menu after action
+      }
+  });
+
+  deleteOption.addEventListener('event', () => {
+      const checkboxContainer = event.target.closest('.checkbox-container');
+      if (checkboxContainer) {
+          const id = checkboxContainer.id.replace('-container', '');
+          removeCheckbox(id);
+          contextMenu.style.display = 'none';  // Hide context menu after action
+      }
+  });
+
+  // Hide the Custom Menu
+  document.addEventListener('click', () => {
+      contextMenu.style.display = 'none';
+  });
+
   });
