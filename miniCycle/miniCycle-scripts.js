7933719ff3e4606bd5f24e82870f3b5033a5c186
mjay0018 <sparkintechproductions@gmail.com>
2025-03-25 04:36:30 -0400

    updated

--- miniCycle/miniCycle-scripts.js
+++ miniCycle/miniCycle-scripts.js
@@ -246,7 +246,7 @@ document.getElementById("close-games-panel").addEventListener("click", () => {
 document.getElementById("open-task-order-game").addEventListener("click", () => {
     // Load game into container or open in new modal
 
-        window.location.href = "/miniCycleGames/miniCycle-taskOrder.html";
+        window.location.href = "/miniCycle/miniCycleGames/miniCycle-taskOrder.html";
    
 });
 /*
